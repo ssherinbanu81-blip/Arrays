fee0ad939b9c0808a2ef517506a009914a6698d3
S.Sherin Banu <ssherinbanu81@gmail.com>
2026-07-30 18:56:45 +0530

    Implement array reversal in reverse.java

---
+++ reverse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static void reverseArray(int a[], int n) {
+        for(int i=n-1;i>=0;i--) {
+            System.out.print(a[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int a[]=new int[n];
+
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+
+        reverseArray(a,n);
+    }
+}
