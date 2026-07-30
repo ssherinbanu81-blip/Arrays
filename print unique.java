d08f494caffc2bc94ab46811a8d91644660d6a5b
S.Sherin Banu <ssherinbanu81@gmail.com>
2026-07-30 19:10:25 +0530

    Create print unique.java

---
+++ print unique.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static void unique(int a[], int n) {
+
+        for(int i=0;i<n;i++) {
+
+            int count=0;
+
+            for(int j=0;j<n;j++) {
+                if(a[i]==a[j])
+                    count++;
+            }
+
+            if(count==1)
+                System.out.print(a[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int a[]=new int[n];
+
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+
+        unique(a,n);
+    }
+}
