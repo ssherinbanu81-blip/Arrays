cbdfcc76d87fbea282c01b007c7a8f49a0372487
S.Sherin Banu <ssherinbanu81@gmail.com>
2026-07-30 19:01:33 +0530

    Implement negative count functionality in Main class

---
+++ negative count.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static void negativeCount(int a[], int n) {
+
+        int count=0;
+
+        for(int i=0;i<n;i++) {
+            if(a[i]<0)
+                count++;
+        }
+
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int a[]=new int[n];
+
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+
+        negativeCount(a,n);
+    }
+}
